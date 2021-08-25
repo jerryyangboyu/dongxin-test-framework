ca18c2f8490dc8fd1713e261a0be521c7b6d338c
yangboyu <yangboyu@yangboyu.net>
2021-08-25 16:49:00 +0800

    adapt project

--- src/main/java/pro/boyu/dongxin/framework/TestApplication.java
+++ src/main/java/pro/boyu/dongxin/framework/TestApplication.java
@@ -1,11 +1,25 @@
 package pro.boyu.dongxin.framework;
 
+import pro.boyu.dongxin.framework.annotations.ExportLogs;
 import pro.boyu.dongxin.utils.logger.LoggerFactory;
+import pro.boyu.dongxin.utils.logger.Reporter;
 
 public class TestApplication {
     public static void run(Class<?> c) {
         LoggerFactory.debug = false;
         OrderedPackageLoader.run(c);
-        System.out.println(LoggerFactory.getMap());
+        if (c.isAnnotationPresent(ExportLogs.class)) {
+            ExportLogs logAnnotation = c.getAnnotation(ExportLogs.class);
+            Reporter reporter = new Reporter(logAnnotation.path(), LoggerFactory.getMap());
+            try {
+                if (logAnnotation.fileType().equals("pdf")) {
+                    reporter.exportPdf();
+                } else {
+                    reporter.exportHtml(logAnnotation.path());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/pro/boyu/dongxin/utils/logger/TestCaseInfoUtil.java
+++ src/main/java/pro/boyu/dongxin/utils/logger/TestCaseInfoUtil.java
@@ -1,5 +1,7 @@
 package pro.boyu.dongxin.utils.logger;
 
+import pro.boyu.dongxin.framework.constenum.TestCaseState;
+
 import java.text.DateFormat;
 import java.text.FieldPosition;
 import java.text.ParsePosition;
@@ -8,8 +10,8 @@ import java.util.Date;
 
 public class TestCaseInfoUtil {
 	
-	public static String executeExceptionInfoUtil(String testClass, String methodNameString, int repeatTime, String exceptionString) {
-		return String.format("test class %s test case %s appears Exeption in %s, after executed %d time ,because of %s",testClass,  methodNameString, new Date(System.currentTimeMillis()),repeatTime,exceptionString);
+	public static String executeExceptionInfoUtil(String testClass, String methodNameString, int repeatTime, String message) {
+		return String.format("Test Class: %s Test Method: %s execute finish in %sms with state %s message %s", testClass, methodNameString, new Date(System.currentTimeMillis()), TestCaseState.SUCCESSFIN, message);
 	}
 
 
