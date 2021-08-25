aaf845f287912dcc1b17bf7e6772aa51d6733d40
yangboyu <yangboyu@yangboyu.net>
2021-08-25 14:54:23 +0800

    enhance logger function

--- src/test/java/pro/boyu/test/GeneralTestClass.java
+++ src/test/java/pro/boyu/test/GeneralTestClass.java
@@ -1,7 +1,12 @@
 package pro.boyu.test;
 
+import pro.boyu.dongxin.framework.annotations.CaseSubject;
 import pro.boyu.dongxin.framework.annotations.TestClass;
 import pro.boyu.dongxin.framework.annotations.TestMethod;
+import pro.boyu.dongxin.framework.constenum.TestCaseState;
+import pro.boyu.dongxin.framework.infobean.ExecutionInfo;
+import pro.boyu.dongxin.framework.subscription.ExecutorSubject;
+import pro.boyu.dongxin.framework.subscription.Subject;
 
 @TestClass
 public class GeneralTestClass {
@@ -22,5 +27,10 @@ public class GeneralTestClass {
     @TestMethod
     void testInstanceBean(TestService service) {
         System.out.println(service.getClient());
+    }
+
+    @TestMethod
+    void testMessage(@CaseSubject ExecutorSubject subject) {
+        subject.updateData(new ExecutionInfo(System.currentTimeMillis(), TestCaseState.RUNNING, "Good"));
     }
 }
